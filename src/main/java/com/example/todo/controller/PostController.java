4bb2c9f9ddb93067307c833ae53afa8eab1ec367
forest-6 <seungmin3238@gmail.com>
2026-03-24 11:56:31 +0900

    feat: 게시글 단건 조회, 게시글 수정, 게시글 삭제 API 추가

--- src/main/java/com/example/todo/controller/PostController.java
+++ src/main/java/com/example/todo/controller/PostController.java
@@ -1,10 +1,7 @@
 package com.example.todo.controller;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -21,17 +18,31 @@ public class PostController {
     @PostMapping("/posts")
     public void createPost(@RequestBody Map<String, String> body) {
         String sql = "INSERT INTO posts (title, content) VALUES (?,?)";
-        jdbcTemplate.update(
-                sql,
-                body.get("title"),
-                body.get("content")
-        );
+        jdbcTemplate.update(sql, body.get("title"), body.get("content"));
     }
 
     @GetMapping("/posts")
     public List<Map<String, Object>> getPosts() {
         String sql = "SELECT * FROM posts";
         return jdbcTemplate.queryForList(sql);
+    }
+
+    @GetMapping("/posts/{id}")
+    public Map<String, Object> getPost(@PathVariable Long id) {
+        String sql = "SELECT * FROM posts WHERE id = ?";
+        return jdbcTemplate.queryForMap(sql, id);
+    }
+
+    @PutMapping("/posts/{id}")
+    public void updatePost(@PathVariable Long id, @RequestBody Map<String, String> body) {
+        String sql = "UPDATE posts SET title = ?, content = ? WHERE id = ?";
+        jdbcTemplate.update(sql, body.get("title"), body.get("content"), id);
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void deletePost(@PathVariable Long id) {
+        String sql = "DELETE FROM posts WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 }
 
