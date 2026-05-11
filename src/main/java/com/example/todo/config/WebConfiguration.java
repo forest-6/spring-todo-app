54371ca1000be280b599c896c49ff0635329fcde
forest-6 <seungmin3238@gmail.com>
2026-05-11 11:38:12 +0900

    회원가입 및 로그인 API 보안 예외 설정

--- src/main/java/com/example/todo/config/WebConfiguration.java
+++ src/main/java/com/example/todo/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.example.todo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -34,7 +35,12 @@ public class WebConfiguration {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults())
-                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/*/users/signup", "/api/*/users/signin")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .sessionManagement(
                         (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(CsrfConfigurer::disable)
