4def0c1f3c510380152d933db720c6fa1b2972e3
forest-6 <seungmin3238@gmail.com>
2026-05-12 11:39:03 +0900

    게시글 api 응답 형식 통일

--- src/main/java/com/example/todo/controller/PostController.java
+++ src/main/java/com/example/todo/controller/PostController.java
@@ -1,11 +1,11 @@
 package com.example.todo.controller;
 
-import com.example.todo.common.ApiResponse;
 import com.example.todo.domain.UserEntity;
 import com.example.todo.dto.post.PostCreateRequest;
 import com.example.todo.dto.post.PostResponse;
 import com.example.todo.dto.post.PostUpdateRequest;
 import com.example.todo.service.PostService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,61 +22,41 @@ public class PostController {
     }
 
     @PostMapping
-    public ApiResponse<Void> createPost(
+    public ResponseEntity<Void> createPost(
             @RequestBody PostCreateRequest request,
             @AuthenticationPrincipal UserEntity user) {
         service.createPost(new PostCreateRequest(request.title(), request.content(), user.getId()));
 
-        return new ApiResponse<>(
-                "SUCCESS",
-                "등록 성공",
-                null
-        );
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public ApiResponse<List<PostResponse>> getPosts() {
-        return new ApiResponse<>(
-                "SUCCESS",
-                "조회 성공",
-                service.getAllPosts()
-        );
+    public ResponseEntity<List<PostResponse>> getPosts() {
+        return ResponseEntity.ok(service.getAllPosts());
     }
 
     @GetMapping("/{id}")
-    public ApiResponse<PostResponse> getPost(@PathVariable Long id) {
-        return new ApiResponse<>(
-                "SUCCESS",
-                "조회 성공",
-                service.getPost(id)
-        );
+    public ResponseEntity<PostResponse> getPost(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getPost(id));
     }
 
     @PutMapping("/{id}")
-    public ApiResponse<Void> updatePost(
+    public ResponseEntity<Void> updatePost(
             @PathVariable Long id,
             @RequestBody PostUpdateRequest request,
             @AuthenticationPrincipal UserEntity user
     ) {
         service.updatePost(new PostUpdateRequest(id, request.title(), request.content()), user.getId());
 
-        return new ApiResponse<>(
-                "SUCCESS",
-                "수정 성공",
-                null
-        );
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ApiResponse<Void> deletePost(
+    public ResponseEntity<Void> deletePost(
             @PathVariable Long id,
             @AuthenticationPrincipal UserEntity user) {
         service.deletePost(id, user.getId());
 
-        return new ApiResponse<>(
-                "SUCCESS",
-                "삭제 성공",
-                null
-        );
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/todo/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.todo.exception;
 
-import com.example.todo.common.ApiResponse;
 import com.example.todo.dto.error.ClientErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,15 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class) // 모든 예외가 발생하면 이 메서드 실행
-    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+    public ResponseEntity<ResponseEntity<Void>> handleException(Exception e) {
         // HTTP 응답 전체를 직접 만드는 객체
-        return ResponseEntity.status(500).body(
-                new ApiResponse<>(
-                        "SERVER_ERR",
-                        "서버 에러 발생",
-                        null
-                )
-        );
+        return ResponseEntity.status(500).build();
     }
 
     @ExceptionHandler(ClientErrorException.class) // 클라이언트 측 에러(4xx)를 공통으로 처리
